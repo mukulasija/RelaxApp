0ce8c979a10fb23a48a253a147056c0cc34abea5
mukulasija <lit2020010@iiitl.ac.in>
2021-11-16 00:28:42 +0530

    side drawer done without fragments

---
+++ app/src/main/java/com/example/relax/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.relax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarToggle : ActionBarDrawerToggle
+    private lateinit var navView : NavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+
+        actionBarToggle = ActionBarDrawerToggle(this,drawerLayout,0,0);
+        drawerLayout.addDrawerListener(actionBarToggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBarToggle.syncState()
+        navView = findViewById(R.id.navView)
+        navView.setNavigationItemSelectedListener {menuItem->
+            when (menuItem.itemId) {
+                R.id.myProfile -> {
+                    Toast.makeText(this, "My Profile", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.people -> {
+                    Toast.makeText(this, "People", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.settings -> {
+                    Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        drawerLayout.openDrawer(navView)
+        return true
+    }
+
+    // override the onBackPressed() function to close the Drawer when the back button is clicked
+    override fun onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
